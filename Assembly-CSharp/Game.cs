c3891671f4ed1c33a96455f7adf43c70b8c0b03a
KooperL <kooper.lingohr@gmail.com>
2025-08-13 03:30:01 -0700

    POC streaming player data

--- Assembly-CSharp/Game.cs
+++ Assembly-CSharp/Game.cs
@@ -2,17 +2,17 @@
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
-// Token: 0x02000174 RID: 372
+// Token: 0x0200011C RID: 284
 public class Game : MonoBehaviour
 {
 	// Token: 0x14000002 RID: 2
-	// (add) Token: 0x06000701 RID: 1793 RVA: 0x00032E1C File Offset: 0x0003101C
-	// (remove) Token: 0x06000702 RID: 1794 RVA: 0x00032E50 File Offset: 0x00031050
+	// (add) Token: 0x060005DB RID: 1499
+	// (remove) Token: 0x060005DC RID: 1500
 	public static event Action onEnterDialogue;
 
-	// Token: 0x170000AD RID: 173
-	// (get) Token: 0x06000703 RID: 1795 RVA: 0x00032E84 File Offset: 0x00031084
-	// (set) Token: 0x06000704 RID: 1796 RVA: 0x000071EE File Offset: 0x000053EE
+	// Token: 0x1700004B RID: 75
+	// (get) Token: 0x060005DD RID: 1501
+	// (set) Token: 0x060005DE RID: 1502
 	public static GameState State
 	{
 		get
@@ -42,8 +42,8 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x170000AE RID: 174
-	// (get) Token: 0x06000705 RID: 1797 RVA: 0x00007216 File Offset: 0x00005416
+	// Token: 0x1700004C RID: 76
+	// (get) Token: 0x060005DF RID: 1503
 	public static bool AllowedToSave
 	{
 		get
@@ -52,9 +52,9 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x170000AF RID: 175
-	// (get) Token: 0x06000706 RID: 1798 RVA: 0x00007245 File Offset: 0x00005445
-	// (set) Token: 0x06000707 RID: 1799 RVA: 0x00007257 File Offset: 0x00005457
+	// Token: 0x1700004D RID: 77
+	// (get) Token: 0x060005E0 RID: 1504
+	// (set) Token: 0x060005E1 RID: 1505
 	public static WorldState WorldState
 	{
 		get
@@ -67,8 +67,28 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x170000B0 RID: 176
-	// (get) Token: 0x06000708 RID: 1800 RVA: 0x00007264 File Offset: 0x00005464
+	// Token: 0x1700004E RID: 78
+	// (get) Token: 0x060005E2 RID: 1506
+	public static int NewGameIndex
+	{
+		get
+		{
+			return GameData.g.ReadInt("NewGameIndex", 0);
+		}
+	}
+
+	// Token: 0x1700004F RID: 79
+	// (get) Token: 0x060005E3 RID: 1507
+	public static bool IsNewGamePlus
+	{
+		get
+		{
+			return Game.NewGameIndex != 0;
+		}
+	}
+
+	// Token: 0x17000050 RID: 80
+	// (get) Token: 0x060005E4 RID: 1508
 	public static bool IsInDialogue
 	{
 		get
@@ -77,8 +97,8 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x170000B1 RID: 177
-	// (get) Token: 0x06000709 RID: 1801 RVA: 0x0000727A File Offset: 0x0000547A
+	// Token: 0x17000051 RID: 81
+	// (get) Token: 0x060005E5 RID: 1509
 	public static bool HasControl
 	{
 		get
@@ -87,9 +107,9 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x170000B2 RID: 178
-	// (get) Token: 0x0600070A RID: 1802 RVA: 0x00007297 File Offset: 0x00005497
-	// (set) Token: 0x0600070B RID: 1803 RVA: 0x00032ED8 File Offset: 0x000310D8
+	// Token: 0x17000052 RID: 82
+	// (get) Token: 0x060005E6 RID: 1510
+	// (set) Token: 0x060005E7 RID: 1511
 	public static int DialogueDepth
 	{
 		get
@@ -110,14 +130,14 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x0600070C RID: 1804 RVA: 0x000072A3 File Offset: 0x000054A3
+	// Token: 0x060005E8 RID: 1512
 	private void Awake()
 	{
 		Game.g = this;
 		this.dialogueDepth = 0;
 	}
 
-	// Token: 0x0600070D RID: 1805 RVA: 0x000072B2 File Offset: 0x000054B2
+	// Token: 0x060005E9 RID: 1513
 	private void OnEnable()
 	{
 		Game.g = this;
@@ -127,30 +147,31 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x0600070E RID: 1806 RVA: 0x00002229 File Offset: 0x00000429
+	// Token: 0x060005EA RID: 1514
 	private void Start()
 	{
+		base.gameObject.AddComponent<PlayerPositionStreamer>();
 	}
 
-	// Token: 0x0600070F RID: 1807 RVA: 0x000072DB File Offset: 0x000054DB
+	// Token: 0x060005EB RID: 1515
 	public void SetToStory()
 	{
 		this.SetWorldState(WorldState.Story);
 	}
 
-	// Token: 0x06000710 RID: 1808 RVA: 0x000072E5 File Offset: 0x000054E5
+	// Token: 0x060005EC RID: 1516
 	public void SetToFlashback()
 	{
 		this.SetWorldState(WorldState.Flashback);
 	}
 
-	// Token: 0x06000711 RID: 1809 RVA: 0x000072EF File Offset: 0x000054EF
+	// Token: 0x060005ED RID: 1517
 	public void SetWorldState(WorldState newWorldState)
 	{
 		this.SetWorldState(newWorldState, false, false);
 	}
 
-	// Token: 0x06000712 RID: 1810 RVA: 0x00032F30 File Offset: 0x00031130
+	// Token: 0x060005EE RID: 1518
 	public void SetWorldState(WorldState newWorldState, bool forceChange = false, bool delaySceneChange = false)
 	{
 		if (newWorldState != this.worldState || forceChange)
@@ -180,56 +201,61 @@ public class Game : MonoBehaviour
 		}
 	}
 
-	// Token: 0x0400096A RID: 2410
-	public const string versionID = "1.0.2";
+	// Token: 0x06001EB4 RID: 7860
+	private void Update()
+	{
+	}
 
-	// Token: 0x0400096B RID: 2411
-	public const int versionInt = 34;
+	// Token: 0x0400080E RID: 2062
+	public const string versionID = "1.0.3";
 
-	// Token: 0x0400096D RID: 2413
+	// Token: 0x0400080F RID: 2063
+	public const int versionInt = 35;
+
+	// Token: 0x04000811 RID: 2065
 	public static bool ignoreDialogueDepth = false;
 
-	// Token: 0x0400096E RID: 2414
+	// Token: 0x04000812 RID: 2066
 	public static float setStatePlayTime = -1f;
 
-	// Token: 0x0400096F RID: 2415
+	// Token: 0x04000813 RID: 2067
 	private const float giveControlDelay = 0.1f;
 
-	// Token: 0x04000970 RID: 2416
+	// Token: 0x04000814 RID: 2068
 	public static Game g;
 
-	// Token: 0x04000971 RID: 2417
+	// Token: 0x04000815 RID: 2069
 	public GameState state;
 
-	// Token: 0x04000972 RID: 2418
+	// Token: 0x04000816 RID: 2070
 	public WorldState worldState;
 
-	// Token: 0x04000973 RID: 2419
+	// Token: 0x04000817 RID: 2071
 	public int dialogueDepth;
 
-	// Token: 0x04000974 RID: 2420
+	// Token: 0x04000818 RID: 2072
 	public bool autoChangeWorldState = true;
 
-	// Token: 0x04000975 RID: 2421
+	// Token: 0x04000819 RID: 2073
 	public GameObject storyPlayer;
 
-	// Token: 0x04000976 RID: 2422
+	// Token: 0x0400081A RID: 2074
 	public GameObject flashbackRoot;
 
-	// Token: 0x04000977 RID: 2423
+	// Token: 0x0400081B RID: 2075
 	public Game.SceneID sceneID = Game.SceneID.Other;
 
-	// Token: 0x04000978 RID: 2424
+	// Token: 0x0400081C RID: 2076
 	public AssetReference mainScene;
 
-	// Token: 0x02000175 RID: 373
+	// Token: 0x020003B0 RID: 944
 	public enum SceneID
 	{
-		// Token: 0x0400097A RID: 2426
+		// Token: 0x04001B79 RID: 7033
 		Prologue,
-		// Token: 0x0400097B RID: 2427
+		// Token: 0x04001B7A RID: 7034
 		MainScene,
-		// Token: 0x0400097C RID: 2428
+		// Token: 0x04001B7B RID: 7035
 		Other
 	}
 }
---
+++ Assembly-CSharp/PlayerPositionStreamer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+// Token: 0x020004CF RID: 1231
+public class PlayerPositionStreamer : MonoBehaviour
+{
+	// Token: 0x06001EC2 RID: 7874
+	private void Start()
+	{
+		base.StartCoroutine(this.SendPositionLoop());
+	}
+
+	// Token: 0x06001EC3 RID: 7875
+	private IEnumerator SendPositionLoop()
+	{
+		for (;;)
+		{
+			string json = JsonUtility.ToJson(new PlayerPositionStreamer.PositionData(Player.RawPosition));
+			byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+			UnityWebRequest request = new UnityWebRequest("http://192.168.8.167:8000", "POST");
+			request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+			request.downloadHandler = new DownloadHandlerBuffer();
+			request.SetRequestHeader("Content-Type", "application/json");
+			yield return request.SendWebRequest();
+			if (request.result != UnityWebRequest.Result.Success)
+			{
+				Debug.LogWarning("Failed to send position: " + request.error);
+			}
+			yield return this.wait;
+			request = null;
+		}
+		yield break;
+	}
+
+	// Token: 0x04002056 RID: 8278
+	private const string url = "http://192.168.8.167:8000";
+
+	// Token: 0x04002057 RID: 8279
+	private WaitForSeconds wait = new WaitForSeconds(0.1f);
+
+	// Token: 0x020004D1 RID: 1233
+	[Serializable]
+	private class PositionData
+	{
+		// Token: 0x06001ECB RID: 7883
+		public PositionData(Vector3 pos)
+		{
+			this.x = pos.x;
+			this.y = pos.y;
+			this.z = pos.z;
+		}
+
+		// Token: 0x0400205C RID: 8284
+		public float x;
+
+		// Token: 0x0400205D RID: 8285
+		public float y;
+
+		// Token: 0x0400205E RID: 8286
+		public float z;
+	}
+}
